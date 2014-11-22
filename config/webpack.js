ec88384472ecc31eb9efb7faf2e74c9a8037c661
Wilbert van de Ridder <wilbert.ridder@gmail.com>
2014-11-22 13:06:59 +0100

    Configuration files Travis config update

---
+++ config/webpack.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var webpack = require('webpack');
+
+/**
+ * Get configuration for Webpack
+ *
+ * @see http://webpack.github.io/docs/configuration
+ *      https://github.com/petehunt/webpack-howto
+ *
+ * @param {boolean} release True if configuration is intended to be used in
+ * a release mode, false otherwise
+ * @return {object} Webpack configuration
+ */
+module.exports = function(release) {
+  return {
+    entry: './src/app.js',
+
+    output: {
+      filename: 'app.js',
+      path: './build/js/',
+      publicPatch: './build/'
+    },
+
+    cache: !release,
+    debug: !release,
+    devtool: false,
+
+    stats: {
+      colors: true,
+      reasons: !release
+    },
+
+    plugins: release ? [
+      new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin()/*,
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+      })*/
+    ] : [/*
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+      })*/],
+
+    resolve: {
+      extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx'],
+      modulesDirectories: ['src', 'node_modules', "src/helper"]
+    },
+
+    module: {
+      preLoaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'jshint'
+        }
+      ],
+
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: 'style!css'
+        },
+        {
+          test: /\.less$/,
+          loader: 'style!css!less'
+        },
+        {
+          test: /\.gif/,
+          loader: 'url-loader?limit=10000&mimetype=image/gif'
+        },
+        {
+          test: /\.jpg/,
+          loader: 'url-loader?limit=10000&mimetype=image/jpg'
+        },
+        {
+          test: /\.png/,
+          loader: 'url-loader?limit=10000&mimetype=image/png'
+        },
+        {
+          test: /\.jsx?$/,
+          loader: 'jsx-loader?harmony'
+        },
+        {
+          test: /\.json/,
+          loader: 'json-loader'
+        }
+      ]
+    }
+  };
+};
