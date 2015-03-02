8bf4052f4f9d2e0f83716a0ba7d06aa8b70b6fd2
Aria Buckles <aria@khanacademy.org>
2015-03-02 01:01:43 -0800

    [markdown] make markdown parsing closer to github's
    Summary:
    By default, simple-markdown ends rules with a blank line
    (two newlines). Github allows ending most rules with a
    single newline, which affects headings in the README here.
    
    This commit removes the double newline requirement from
    the heading parsing. It also adds an extra newline around
    either side of the code block so that it parses as a block
    rather than an inline code element, and makes the markdown
    output function output <p> tags instead of
    <div class="paragraph"> tags.
    
    Test Plan:
    1. run `npm install`
    2. run `cd server && node src/server.js`
    3. load http://localhost:8080/ and http://localhost:8080/about
    4. compare their output; see that the headings and code
       look the same in both, and that the output matches at
       various points in each page.

---
+++ src/components/markdown/custom-md.jsx
@@ -0,0 +1,45 @@
+var $ = require("jquery");
+var React = require("react");
+var SimpleMarkdown = require("simple-markdown");
+
+var blockRegex = SimpleMarkdown.blockRegex;
+var defaultRules = SimpleMarkdown.defaultRules;
+
+var rules = $.extend({}, defaultRules, {
+    // Close headings and lheadings with a single newline, instead
+    // of requiring a blank line after them.
+    //
+    // For heading, this changes:
+    //    /^ *(#{1,6}) *([^\n]+?) *#* *(?:\n *)+\n/
+    // to /^ *(#{1,6}) *([^\n]+?) *#* *(?:\n *)*\n/
+    // For lheading, this changes:
+    //    /^([^\n]+)\n *(=|-){3,} *(?:\n *)+\n/
+    // to /^([^\n]+)\n *(=|-){3,} *(?:\n *)*\n/
+    // (Specifically, note the `+` to `*` change at the end of each)
+    heading: $.extend({}, defaultRules.heading, {
+        match: blockRegex(/^ *(#{1,6}) *([^\n]+?) *#* *(?:\n *)*\n/)
+    }),
+    lheading: $.extend({}, defaultRules.lheading, {
+        match: blockRegex(/^([^\n]+)\n *(=|-){3,} *(?:\n *)*\n/)
+    }),
+
+    // Make paragraphs output raw <p> tags, instead of
+    // <div class="paragraph"> tags:
+    paragraph: $.extend({}, defaultRules.paragraph, {
+        output: function(node, output) {
+            return <p>{output(node.content)}</p>;
+        }
+    })
+});
+
+var rawBuiltParser = SimpleMarkdown.parserFor(rules);
+var parse = function(source) {
+    var blockSource = source + "\n\n";
+    return rawBuiltParser(blockSource, {inline: false});
+};
+var output = SimpleMarkdown.outputFor(SimpleMarkdown.ruleOutput(rules));
+
+module.exports = {
+    parse: parse,
+    output: output
+};
